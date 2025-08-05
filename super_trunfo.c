7fb37285e64f58944bb8b832f6b36011a088c9eb
Capitu-dev <mandiza22@gmail.com>
2025-08-04 23:18:36 -0300

    CORRIGIDO super_trunfo.c
    adc comentarios

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
-    char Estado;
+    char Estado; // char: armazena caracteres, letras. OU strings, palavras.
     char Codigo[10];
     char Cidade[50];
-    int População;
-    float Area;
+    int População; // int: valor inteiro 
+    float Area; // ponto flutuante, trabalhar com casas decimais, podemos adicionar virgulas.
     float PIB;
     int PontosTuristicos;
 
     printf("Digite seu estado: \n");
-    scanf("%s", &Estado);
+    scanf("%s", &Estado); // %s: para uma sequencia de letras/caracteres.
 
     printf("Digite o codigo da carta: \n");
     scanf("%s", &Codigo);
@@ -18,10 +18,10 @@ int main(){
     scanf("%s", &Cidade);
 
     printf("Digite a populção: \n");
-    scanf("%d", &População);
+    scanf("%d", &População); // %d para formato decimal 
 
     printf("Digite a área: \n");
-    scanf("%f", &Area);
+    scanf("%f", &Area); // %f para numero com ponto
 
     printf("Digite o PIB: \n");
     scanf("%f", &PIB);
