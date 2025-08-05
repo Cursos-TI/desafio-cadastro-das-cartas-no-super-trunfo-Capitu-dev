427dc6c1b8e678a686f8117e9f037a583bedaf8f
Capitu-dev <mandiza22@gmail.com>
2025-08-04 23:10:44 -0300

    CORRIGIDO super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,85 +1,33 @@
 #include <stdio.h>
-#include <string.h>
+int main(){
+    char Estado;
+    char Codigo[10];
+    char Cidade[50];
+    int População;
+    float Area;
+    float PIB;
+    int PontosTuristicos;
 
-void ler_carta();  // declara função
-void ler_carta2();
+    printf("Digite seu estado: \n");
+    scanf("%s", &Estado);
 
-int main() { //professor, tive que pesquisar isso pq nao tava conseguindo colocar as duas cartao em um programa só. 
-    printf("==== CARTA 1 ====\n");
-    ler_carta();
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &Codigo);
 
-    printf("\n==== CARTA 2 ====\n");
-    ler_carta2();
+    printf("Digite a sua cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite a populção: \n");
+    scanf("%d", &População);
+
+    printf("Digite a área: \n");
+    scanf("%f", &Area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite quantidade pontos turisticos: \n");
+    scanf("%d", &PontosTuristicos);
 
     return 0;
-}
-
-void ler_carta() {
-    char estado;
-    char codigo_da_carta[10];
-    char nome_da_cidade[50];
-    int populacao;
-    float area_em_KM;
-    float PIB;
-    int pontos_turisticos;
-
-    printf("Digite seu estado: \n");
-    scanf(" %c", &estado);  
-
-    printf("Codigo da carta: \n");
-    scanf("%s", codigo_da_carta);
-
-    getchar();  // tive que pesquisar isso tbm pra entender pq eu nao tava conseguindo colocar nome composto das cidades
-
-    printf("Digite o nome da cidade: \n");
-    fgets(nome_da_cidade, sizeof(nome_da_cidade), stdin);
-    nome_da_cidade[strcspn(nome_da_cidade, "\n")] = 0;  
-
-    printf("Digite quantidade de populacao: \n");
-    scanf("%d", &populacao);
-
-    printf("Digite a area em KM: \n");
-    scanf("%f", &area_em_KM);
-
-    printf("Digite o PIB da sua cidade: \n");
-    scanf("%f", &PIB);
-
-    printf("Digite pontos turisticos da cidade: \n");
-    scanf("%d", &pontos_turisticos);
-}
-
-void ler_carta2() { //pesquisei sobre void tbm pra poder separar as 2 cartas
-    char estado;
-    char codigo_da_carta[10];
-    char nome_da_cidade[50];
-    int populacao;
-    float area_em_KM;
-    float PIB;
-    int pontos_turisticos;
-
-    printf("Digite seu estado: \n");
-    scanf(" %c", &estado);  
-
-    printf("Codigo da carta: \n");
-    scanf("%s", codigo_da_carta);
-
-    getchar();  
-
-    printf("Digite o nome da cidade: \n");
-    fgets(nome_da_cidade, sizeof(nome_da_cidade), stdin);
-    nome_da_cidade[strcspn(nome_da_cidade, "\n")] = 0;  
-
-    printf("Digite quantidade de populacao: \n");
-    scanf("%d", &populacao);
-
-    printf("Digite a area em KM: \n");
-    scanf("%f", &area_em_KM);
-
-    printf("Digite o PIB da sua cidade: \n");
-    scanf("%f", &PIB);
-
-    printf("Digite pontos turisticos da cidade: \n");
-    scanf("%d", &pontos_turisticos);
-
-    return 0; 
-}
+    }
